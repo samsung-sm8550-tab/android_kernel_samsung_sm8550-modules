e3e8a0c8c75af4f29b029735a23697e1ab1f7383
Bruno Martins <bgcngm@gmail.com>
2025-07-21 18:37:27 +0100

    Merge tag 'AUDIO.LA.8.0.r1-12500-KAILUA.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/audio-kernel-ar into HEAD
    AUDIO.LA.8.0.r1-12500-KAILUA.0
    
    * tag 'AUDIO.LA.8.0.r1-12500-KAILUA.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/audio-kernel-ar:
      asoc: codec: avoid out of bound read while checking a bit
      asoc: kalama: Add device in dai_link for kalama_iot_vc platform
      asoc: codec: wcd939x: avoid out of bound write to map array
      asoc: codec: avoid out of bound write to map array
    
    Change-Id: I57b1091f510aabd8c4ad595c22d671b0275fc313

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <sound/soc.h>
@@ -407,6 +407,15 @@ SND_SOC_DAILINK_DEFS(tavil_i2s_tx1,
 	DAILINK_COMP_ARRAY(COMP_CODEC("tavil_codec", "tavil_i2s_tx1")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
+SND_SOC_DAILINK_DEFS(iot_vc_sep_mi2s_rx,
+	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
+	DAILINK_COMP_ARRAY(
+		COMP_CODEC("TAS5805m.0-002d", "TAS5805m.0-002d"),
+		COMP_CODEC("add2010.0-0034", "add2010"),
+		COMP_CODEC("lt9611.2-002b", "lt9611")
+		),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
+
 #ifdef CONFIG_COMMON_AMP_CIRRUS
 SND_SOC_DAILINK_DEFS(cirrus_pri_tdm_rx_0,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
@@ -440,3 +449,4 @@ SND_SOC_DAILINK_DEFS(goodix_pri_tdm_tx_0,
 		COMP_CODEC("tfa98xx.18-0035", "tfa98xx-aif-18-35")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 #endif
+
